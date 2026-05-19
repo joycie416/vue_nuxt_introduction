60f7652b31f1f9b405ed5701710abbd896893e1f
joycie416 <joycie416@gmail.com>
2026-05-19 17:52:41 +0900

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/test-utils'
+  ]
 })
